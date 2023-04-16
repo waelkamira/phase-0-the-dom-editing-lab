7b9d1ae5865a21a89f8da2f44bae9cc32e2f84a4
wael kamira <ramondm4@gmail.com>
2023-04-16 17:02:22 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+// document.querySelector("html").innerHTML = "<body>";
+// document.querySelector("html").innerHTML = "</body>";
+
+// document.querySelector("body").innerHTML = "<h1>My HTML adventure</h1>";
+// document.querySelector("body").innerHTML = `"<p>
+// We\'re writing HTML markup to display in our <strong>browser</strong>. We\'re
+// basically telling computers what to do. <em>Neat!</em>
+// </p>"`;
+// document.querySelector("body").innerHTML = "table";
+
+// document.querySelector("p").innerHTML = '<a href="https://developer.mozilla.org/en-US/docs/Web/HTML">';
+
+// document.querySelector("p").innerHTML = "<strong>";
+
+// document.querySelector("p").innerHTML = "<em>";
+
